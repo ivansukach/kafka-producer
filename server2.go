203652ab3630a5378593c85476590faf76786f04
ivansukach <vsukach1999@gmail.com>
2020-03-05 22:38:32 +0300

    Latest stable version 2

--- server2.go
+++ server2.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	topic := "my13topic"
 	partition := 0
-
+	selector := true
 	conn, _ := kafka.DialLeader(context.Background(), "tcp", "localhost:9092", topic, partition)
 	defer conn.Close()
 	tRole := time.NewTicker(time.Second * 30)
@@ -19,14 +19,39 @@ func main() {
 	for {
 		select {
 		case <-tRole.C:
-			fmt.Println("Time is over")
+			if selector {
+				log.Println("Time is over")
+				_, err := conn.WriteMessages(
+					kafka.Message{Value: []byte(fmt.Sprintf("server1:producer"))},
+				)
+				if err != nil {
+					log.Error(err)
+				}
+				log.Println("Event on server2 : selector->consume")
+				selector = false
+			} else {
+				log.Println("Time is over")
+				_, err := conn.WriteMessages(
+					kafka.Message{Value: []byte(fmt.Sprintf("server2:producer"))},
+				)
+				if err != nil {
+					log.Error(err)
+				}
+				log.Println("Event on server2 : selector->produce")
+				selector = true
+			}
+
 		case <-tMes.C:
-			fmt.Println("write")
-			_, err := conn.WriteMessages(
-				kafka.Message{Value: []byte(fmt.Sprintf("server2 say: %s", time.Now().UTC().String()))},
-			)
-			if err != nil {
-				log.Error(err)
+			if selector {
+				log.Println("write")
+				_, err := conn.WriteMessages(
+					kafka.Message{Value: []byte(fmt.Sprintf("server2 say: %s", time.Now().UTC().String()))},
+				)
+				if err != nil {
+					log.Error(err)
+				}
+			} else {
+				log.Println("read")
 			}
 		}
 	}
