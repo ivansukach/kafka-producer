0446432c801e7d083c2406fc8e8fc5e7047c2ae6
ivansukach <vsukach1999@gmail.com>
2020-03-08 00:37:05 +0300

    Stable version

--- server2.go
+++ server2.go
@@ -3,15 +3,57 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 	"github.com/segmentio/kafka-go"
 	log "github.com/sirupsen/logrus"
+	"golang.org/x/net/websocket"
 	"time"
 )
 
+func changeRole(c echo.Context, selector *bool) error {
+	websocket.Handler(func(ws *websocket.Conn) {
+		defer ws.Close()
+		for {
+			log.Println("Handler")
+			msg := ""
+			err := websocket.Message.Receive(ws, &msg)
+			if err != nil {
+				c.Logger().Error(err)
+			}
+			fmt.Printf("%s\n", msg)
+			switch msg {
+			case "server2:producer":
+				log.Println("Let's produce messages")
+				*selector = true
+			}
+		}
+	}).ServeHTTP(c.Response(), c.Request())
+	return nil
+}
+
 func main() {
-	topic := "my13313topic"
-	partition := 0
 	selector := true
+	go messageExchange(&selector)
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Static("/", "public")
+	e.GET("/ws", func(c echo.Context) error {
+		return changeRole(c, &selector)
+	})
+	e.Logger.Fatal(e.Start(":1323"))
+
+}
+func messageExchange(selector *bool) {
+	origin := "http://localhost/"
+	url := "ws://localhost:1333/ws"
+	ws, err := websocket.Dial(url, "", origin)
+	if err != nil {
+		log.Fatal(err)
+	}
+	topic := "my100topic"
+	partition := 0
 	conn, _ := kafka.DialLeader(context.Background(), "tcp", "localhost:9092", topic, partition)
 	defer conn.Close()
 	tRole := time.NewTicker(time.Second * 30)
@@ -19,22 +61,15 @@ func main() {
 	for {
 		select {
 		case <-tRole.C:
-			if selector {
-				log.Println("Time is over")
-				_, err := conn.WriteMessages(
-					kafka.Message{Value: []byte(fmt.Sprintf("server1:producer"))},
-				)
-				if err != nil {
-					log.Error(err)
+			if *selector {
+				if _, err := ws.Write([]byte("server1:producer")); err != nil {
+					log.Fatal(err)
 				}
-				log.Println("Event on server2 : selector->consume")
-				selector = false
-			} else {
-				log.Println("Time to read is over")
+				log.Println("Let's read messages")
+				*selector = false
 			}
-
 		case <-tMes.C:
-			if selector {
+			if *selector {
 				log.Println("write")
 				_, err := conn.WriteMessages(
 					kafka.Message{Value: []byte(fmt.Sprintf("server2 say: %s", time.Now().UTC().String()))},
@@ -43,8 +78,9 @@ func main() {
 					log.Error(err)
 				}
 			}
-			if !selector {
+			if !*selector {
 				log.Println("read")
+				conn.SetReadDeadline(time.Now().Add(3 * time.Second))
 				batch := conn.ReadBatch(1, 1e3) // fetch 10KB min, 1MB max
 				for {
 					b := make([]byte, 1e3) // 10KB max per message
@@ -55,11 +91,6 @@ func main() {
 					bb := b[:amount]
 					mes := string(bb)
 					log.Println(mes)
-					if mes == "server2:producer" {
-						log.Println("Event on server2 : selector->produce")
-						selector = true
-						break
-					}
 				}
 				batch.Close()
 			}
